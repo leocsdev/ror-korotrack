2002aa79d5fc74c1ea27413f9e0a3051efda1f31
Leo <leocsdev@gmail.com>
2025-12-17 11:51:47 +0800

    rails db:migrate

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id         :bigint           not null, primary key
+#  active     :boolean          default(TRUE), not null
+#  first_name :string           not null
+#  kapisanan  :string
+#  last_name  :string           not null
+#  voice_part :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_members_on_active  (active)
+#
 require "test_helper"
 
 class MemberTest < ActiveSupport::TestCase
