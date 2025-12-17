445009397dfaad2575d87d188a9c88eff98169d2
Leo <leocsdev@gmail.com>
2025-12-17 12:44:34 +0800

    rails db:migrate

--- db/migrate/20251217044017_create_attendances.rb
+++ db/migrate/20251217044017_create_attendances.rb
@@ -3,9 +3,14 @@ class CreateAttendances < ActiveRecord::Migration[8.1]
     create_table :attendances do |t|
       t.references :member, null: false, foreign_key: true
       t.references :attendance_date, null: false, foreign_key: true
-      t.string :status
+      t.string :status, null: false
 
       t.timestamps
     end
+
+    # Prevents duplicate attendance records
+    # Enforces relational integrity
+    # Makes race conditions harmless
+    add_index :attendances, [:member_id, :attendance_date_id], unique: true, name: "index_attendances_on_member_and_date"
   end
 end
--- test/models/attendance_test.rb
+++ test/models/attendance_test.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: attendances
+#
+#  id                 :bigint           not null, primary key
+#  status             :string           not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  attendance_date_id :bigint           not null
+#  member_id          :bigint           not null
+#
+# Indexes
+#
+#  index_attendances_on_attendance_date_id  (attendance_date_id)
+#  index_attendances_on_member_and_date     (member_id,attendance_date_id) UNIQUE
+#  index_attendances_on_member_id           (member_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (attendance_date_id => attendance_dates.id)
+#  fk_rails_...  (member_id => members.id)
+#
 require "test_helper"
 
 class AttendanceTest < ActiveSupport::TestCase
