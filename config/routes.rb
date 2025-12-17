d103eb217d2621fff1eec7b2f44f9643b07b77cf
Leo <leocsdev@gmail.com>
2025-12-17 16:21:27 +0800

    fix lint error

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "attendances#show"
 
-  resource :attendance, only: [:show]
+  resource :attendance, only: [ :show ]
 end
