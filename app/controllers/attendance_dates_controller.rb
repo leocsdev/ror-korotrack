1f7f91385ab6e70213a299e0e81f5b0b9c7ae26d
Leo <leocsdev@gmail.com>
2025-12-18 12:44:52 +0800

    Added logic for attendance dates index and create actions

--- app/controllers/attendance_dates_controller.rb
+++ app/controllers/attendance_dates_controller.rb
@@ -1,7 +1,22 @@
 class AttendanceDatesController < ApplicationController
   def index
+    @attendance_dates = AttendanceDate.order(date: :desc)
   end
 
   def create
+    attendance_date = AttendanceDate.find_or_create_by!(
+      date: parsed_date,
+      attendance_type: params[:attendance_type]
+    )
+
+    redirect_to attendance_path(date: attendance_date.date)
+  end
+
+  private
+
+  def parsed_date
+    Date.parse(params[:date])
+  rescue ArgumentError
+    Date.current
   end
 end
